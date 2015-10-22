83813b684cc74f29b94c6d6aab36d80d44831af6
Egill Anton Hlöðversson <egilla14@ru.is>
2015-10-22 18:47:02 +0000

    isActive() implemented

--- src/main/java/is/ru/TicTacToe/TicTacToe.java
+++ src/main/java/is/ru/TicTacToe/TicTacToe.java
@@ -1,6 +1,22 @@
 package is.ru.TicTacToe;
 
 public class TicTacToe {
+	public enum STATE {BLANK, CROSS, CIRCLE}
+
+    private STATE[][] grid = new STATE[3][3];
+    private STATE winner;
+
+    public TicTacToe() {
+        for (int x = 0; x < grid.length; x++) {
+            for (int y = 0; y < grid[x].length; y++) {
+                grid[x][y] = STATE.BLANK;
+            }
+        }
+    }
+
+    public boolean isActive(){
+    	return grid != null;
+    }
 
 	public static int add(String text){
 		if(text.equals("")){

6084874038c628dceafe6372ac7782f200515059
Egill Anton Hlöðversson <egilla14@ru.is>
2015-10-22 18:46:10 +0000

    Test - isActive()

--- src/test/java/is/ru/TicTacToe/TicTacToeTest.java
+++ src/test/java/is/ru/TicTacToe/TicTacToeTest.java
@@ -2,6 +2,7 @@ package is.ru.TicTacToe;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.Before;
 
 public class TicTacToeTest {
 
@@ -9,8 +10,16 @@ public class TicTacToeTest {
       org.junit.runner.JUnitCore.main("is.ru.TicTacToe.TicTacToeTest");
     }
 
+    private TicTacToe t;
+
+    @Before
+    public void setUp() {
+        t = new TicTacToe();
+    }
+
+
 	@Test
-	public void testEmptyString() {
-		assertEquals(0 , TicTacToe.add(""));
+	public void testIsActive() {
+		assertEquals(true , t.isActive());
 	}
 }

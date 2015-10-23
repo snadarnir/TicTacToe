5c007c37341378663a5c027099945e6db2ebf217
Egill Anton Hlöðversson <egilla14@ru.is>
2015-10-23 15:20:29 +0000

    Documentation added for code so far

--- src/main/java/is/ru/TicTacToe/TicTacToe.java
+++ src/main/java/is/ru/TicTacToe/TicTacToe.java
@@ -1,3 +1,18 @@
+/*TicTacToe
+Created by: Snadarnir
+University: Reykjavik university - 2015
+Authors:
+    Ari Hrobjartsson
+    Egill Anton Hlodversson
+    Gudrun Thorey Sigurbjornsdottir
+    Kristófer Andri Kristinsson
+    Maria Builien Jonsdottir
+Discription:
+    Java implemented TicTacToe, With focus on Test Driven Developmente, 
+    that the code should be loosely coupled and that the design follow good     
+    object oriented design practices.
+*/
+
 package is.ru.TicTacToe;
 
 public class TicTacToe {
@@ -16,42 +31,52 @@ public class TicTacToe {
         winner = STATE.BLANK;
     }
 
+    // Checks if the object is active
     public boolean isActive(){
     	return grid != null;
     }
 
+    // Sets a Circle in the grid point (x,y)
     public void setCircle(int x, int y){
     	grid[y][x] = STATE.CIRCLE;
     }
 
+    // Sets a Cross in the grid point (x,y)
     public void setCross(int x, int y){
     	grid[y][x] = STATE.CROSS;
     }
 
+    // Sets Blank in the grid point (x,y)
     public void setBlank(int x, int y){
     	grid[y][x] = STATE.BLANK;
     }
 
+    // Returns true if grid point(x,y) is a Circle
     public boolean isCircle(int x, int y){
     	return grid[y][x]  == STATE.CIRCLE;
     }
 
+    // Returns true if grid point(x,y) is a Cross
     public boolean isCross(int x, int y){
         return grid[y][x]  == STATE.CROSS;
     }
 
+    // Returns true if grid point(x,y) is Blank
     public boolean isBlank(int x, int y){
         return grid[y][x]  == STATE.BLANK;
     }
 
+    // Sets the winner of the current game to specific state
     public void setWinner(STATE s){
     	winner = s;
     }
 
+    // Returns true if the winner is a specific state
     public boolean isWinner(STATE s){
     	return winner == s;
     }
 
+    // Restarts the game by cleaning the grid and resets the winner
     public void cleanGrid(){
     	for (int y = 0; y < grid.length; y++) {
             for (int x = 0; x < grid[y].length; x++) {
@@ -62,6 +87,7 @@ public class TicTacToe {
         winner = STATE.BLANK;
     }
 
+    // Returns true if  the game grid is all blank and no winner specified
     public boolean isClean(){
 
     	if(winner != STATE.BLANK) 
@@ -76,6 +102,7 @@ public class TicTacToe {
         return true;
     }
 
+    // Returns true if  there is any Cross or Circle three in a row on the grid
     public boolean isThreeInRow(){
 	for(int x = 0; x < grid.length; x++){
 		STATE first = grid[0][x];
@@ -107,6 +134,7 @@ public class TicTacToe {
 	return false;
     }
     
+    // Returns true if there is any Blank state on the game grid
     public boolean isAnyFreeSpotLeft(){
 	for(int x = 0; x < grid.length; x++){
 		for(int y = 0; y < grid.length; y++){
